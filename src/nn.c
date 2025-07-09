56410732c3d9ba23090040fabcec110c081d7e4b
ynoplanetashka2 <mrynoplanetashka@gmail.com>
2025-07-10 02:34:16 +0300

    removed dummy output layer by default (layers_count now has to be positive) & fix bugs in the learning algorithm

--- src/nn.c
+++ src/nn.c
@@ -3,24 +3,24 @@
 #include <math.h>
 #include "nn.h"
 #include "square_error/square_error.h"
+#include "linear-algebra/real.h"
 
 nn nn_create(nn_layer* layers, unsigned int layers_count, unsigned int input_size, unsigned int output_size) {
-  matrix* weights = (matrix*) malloc(sizeof(matrix) * (layers_count + 1));
+  matrix* weights = (matrix*) malloc(sizeof(matrix) * layers_count);
   unsigned int weights_input_size = input_size;
-  unsigned int weights_output_size = layers_count > 0 ? layers[0].input_size : output_size;
-  for (unsigned int i = 0; i < layers_count; ++i) {
-    weights[i] = matrix_create(weights_input_size, weights_output_size);
-    weights_input_size = layers[i].output_size;
-    weights_output_size = i + 1 < layers_count ? layers[i + 1].input_size : output_size;
+  unsigned int weights_output_size = layers[0].input_size;
+  weights[0] = matrix_create(weights_output_size, weights_input_size);
+  for (unsigned int i = 1; i < layers_count; ++i) {
+    weights_input_size = layers[i - 1].output_size;
+    weights_output_size = layers[i].input_size;
+    weights[i] = matrix_create(weights_output_size, weights_input_size);
   }
-  weights[layers_count] = matrix_create(weights_input_size, weights_output_size);
 
-  vec* bias = (vec*) malloc(sizeof(vec) * (layers_count + 1));
+  vec* bias = (vec*) malloc(sizeof(vec) * layers_count);
   for (unsigned int i = 0; i < layers_count; ++i) {
     const nn_layer layer = layers[i];
     bias[i] = vec_create(layer.input_size);
   }
-  bias[layers_count] = vec_create(output_size);
 
   return (nn) {
     .layers_count = layers_count,
@@ -44,22 +44,21 @@ void nn_free(nn nn_instance) {
 }
 
 nn nn_zeros(nn_layer* layers, unsigned int layers_count, unsigned int input_size, unsigned int output_size) {
-  matrix* weights = (matrix*) malloc(sizeof(matrix) * (layers_count + 1));
+  matrix* weights = (matrix*) malloc(sizeof(matrix) * layers_count);
   unsigned int weights_input_size = input_size;
-  unsigned int weights_output_size = layers_count > 0 ? layers[0].input_size : output_size;
-  for (unsigned int i = 0; i < layers_count; ++i) {
-    weights[i] = matrix_zeros(weights_input_size, weights_output_size);
-    weights_input_size = layers[i].output_size;
-    weights_output_size = i + 1 < layers_count ? layers[i + 1].input_size : output_size;
+  unsigned int weights_output_size = layers[0].input_size;
+  weights[0] = matrix_zeros(weights_output_size, weights_input_size);
+  for (unsigned int i = 1; i < layers_count; ++i) {
+    weights_input_size = layers[i - 1].output_size;
+    weights_output_size = layers[i].input_size;
+    weights[i] = matrix_zeros(weights_output_size, weights_input_size);
   }
-  weights[layers_count] = matrix_zeros(weights_input_size, weights_output_size);
 
-  vec* bias = (vec*) malloc(sizeof(vec) * (layers_count + 1));
+  vec* bias = (vec*) malloc(sizeof(vec) * layers_count);
   for (unsigned int i = 0; i < layers_count; ++i) {
     const nn_layer layer = layers[i];
     bias[i] = vec_zeros(layer.input_size);
   }
-  bias[layers_count] = vec_zeros(output_size);
 
   return (nn) {
     .layers_count = layers_count,
@@ -72,22 +71,21 @@ nn nn_zeros(nn_layer* layers, unsigned int layers_count, unsigned int input_size
 }
 
 nn nn_rand(nn_layer* layers, unsigned int layers_count, unsigned int input_size, unsigned int output_size) {
-  matrix* weights = (matrix*) malloc(sizeof(matrix) * (layers_count + 1));
+  matrix* weights = (matrix*) malloc(sizeof(matrix) * layers_count);
   unsigned int weights_input_size = input_size;
-  unsigned int weights_output_size = layers_count > 0 ? layers[0].input_size : output_size;
-  for (unsigned int i = 0; i < layers_count; ++i) {
-    weights[i] = matrix_rand(weights_input_size, weights_output_size);
-    weights_input_size = layers[i].output_size;
-    weights_output_size = i + 1 < layers_count ? layers[i + 1].input_size : output_size;
+  unsigned int weights_output_size = layers[0].input_size;
+  weights[0] = matrix_rand(weights_output_size, weights_input_size);
+  for (unsigned int i = 1; i < layers_count; ++i) {
+    weights_input_size = layers[i - 1].output_size;
+    weights_output_size = layers[i].input_size;
+    weights[i] = matrix_rand(weights_output_size, weights_input_size);
   }
-  weights[layers_count] = matrix_rand(weights_input_size, weights_output_size);
 
-  vec* bias = (vec*) malloc(sizeof(vec) * (layers_count + 1));
+  vec* bias = (vec*) malloc(sizeof(vec) * layers_count);
   for (unsigned int i = 0; i < layers_count; ++i) {
     const nn_layer layer = layers[i];
     bias[i] = vec_rand(layer.input_size);
   }
-  bias[layers_count] = vec_rand(output_size);
 
   return (nn) {
     .layers_count = layers_count,
@@ -172,13 +170,12 @@ typedef struct {
 } _nn_prediction_result_for_training;
 
 void _nn_free_prediction_result_for_training(_nn_prediction_result_for_training* prediction_results, const unsigned int layers_count) {
-  for (unsigned int i = 0; i < layers_count + 1; ++i) {
+  for (unsigned int i = 0; i < layers_count; ++i) {
     vec_free(prediction_results->nn_prediction_results[i]);
-    if (i < layers_count) {
-      matrix_free(prediction_results->nn_transform_derivative_values[i]);
-    }
+    matrix_free(prediction_results->nn_transform_derivative_values[i]);
   }
   free(prediction_results->nn_prediction_results);
+  free(prediction_results->nn_transform_derivative_values);
 }
 
 _nn_prediction_result_for_training _nn_compute_prediction_result_for_training(
@@ -187,12 +184,12 @@ _nn_prediction_result_for_training _nn_compute_prediction_result_for_training(
 ) {
   const unsigned int layers_count = nn_instance.layers_count;
   vec buffer = vec_copy(input);
-  vec* nn_prediction_results = (vec*) malloc(sizeof(vec) * (layers_count + 1));
+  vec* nn_prediction_results = (vec*) malloc(sizeof(vec) * layers_count);
   matrix* nn_transform_derivative_values = (matrix*) malloc(sizeof(matrix) * layers_count);
   for (unsigned int i = 0; i < nn_instance.layers_count; ++i) {
-    matrix weights = nn_instance.weights[i];
-    vec bias = nn_instance.bias[i];
-    nn_layer layer = nn_instance.layers[i];
+    const matrix weights = nn_instance.weights[i];
+    const vec bias = nn_instance.bias[i];
+    const nn_layer layer = nn_instance.layers[i];
     matrix_apply_inplace(weights, &buffer);
     vec_sum_inplace(&buffer, bias);
     matrix transform_derivative_value = layer.transform_derivative(layer.input_size, layer.output_size, buffer);
@@ -200,160 +197,64 @@ _nn_prediction_result_for_training _nn_compute_prediction_result_for_training(
     vec_assign(&buffer, layer.transform(layer.input_size, layer.output_size, buffer));
     nn_prediction_results[i] = vec_copy(buffer);
   }
-  matrix weights = nn_instance.weights[layers_count];
-  vec bias = nn_instance.bias[layers_count];
-  matrix_apply_inplace(weights, &buffer);
-  vec_sum_inplace(&buffer, bias);
-  nn_prediction_results[layers_count] = buffer;
-  _nn_prediction_result_for_training result = {
+  vec_free(buffer);
+  return (_nn_prediction_result_for_training) {
     .nn_prediction_results = nn_prediction_results,
     .nn_transform_derivative_values = nn_transform_derivative_values
   };
-  return result;
 }
 
 void _nn_fit_once(nn nn_instance, const vec input, const vec expected_output, const float epsilon) {
   const unsigned int layers_count = nn_instance.layers_count;
   _nn_prediction_result_for_training nn_prediction_results = _nn_compute_prediction_result_for_training(nn_instance, input);
   vec current_expected_output = vec_copy(expected_output);
-  matrix identity = matrix_identity(nn_instance.output_size);
 
-  if (layers_count > 0) {
-    {
-      vec bias_gradient = _compute_bias_gradient(
-        nn_instance.weights[nn_instance.layers_count],
-        nn_prediction_results.nn_prediction_results[nn_instance.layers_count - 1],
-        current_expected_output,
-        nn_instance.bias[nn_instance.layers_count],
-        nn_prediction_results.nn_prediction_results[nn_instance.layers_count],
-        identity
-      );
-      vec signal_gradient = _compute_signal_gradient(
-        nn_instance.weights[nn_instance.layers_count],
-        nn_prediction_results.nn_prediction_results[nn_instance.layers_count - 1],
-        current_expected_output,
-        nn_instance.bias[nn_instance.layers_count],
-        nn_prediction_results.nn_prediction_results[nn_instance.layers_count],
-        identity
-      );
-      matrix weights_gradient = _compute_weights_gradient(
-        nn_instance.weights[nn_instance.layers_count],
-        nn_prediction_results.nn_prediction_results[nn_instance.layers_count - 1],
-        current_expected_output,
-        nn_instance.bias[nn_instance.layers_count],
-        nn_prediction_results.nn_prediction_results[nn_instance.layers_count],
-        identity
-      );
-      vec_scalar_multiply_inplace(&bias_gradient, epsilon);
-      matrix_scalar_multiply_inplace(&weights_gradient, epsilon);
-      vec_subtract_inplace(&nn_instance.bias[layers_count], bias_gradient);
-      matrix_subtract_inplace(&nn_instance.weights[layers_count], weights_gradient);
-
-      vec_assign(&current_expected_output, 
-        vec_subtract(nn_prediction_results.nn_prediction_results[layers_count - 1], signal_gradient));
-      vec_free(signal_gradient);
-      vec_free(bias_gradient);
-      matrix_free(weights_gradient);
-    }
-    for (unsigned int i = nn_instance.layers_count - 1; i >= 1; --i) {
-      const vec activation_function_value = nn_prediction_results.nn_prediction_results[i];
-      const matrix activation_function_derivative_value = nn_prediction_results.nn_transform_derivative_values[i];
-      vec bias_gradient = _compute_bias_gradient(
-        nn_instance.weights[i],
-        nn_prediction_results.nn_prediction_results[i - 1],
-        current_expected_output,
-        nn_instance.bias[i],
-        activation_function_value,
-        activation_function_derivative_value
-      );
-      vec signal_gradient = _compute_signal_gradient(
-        nn_instance.weights[i],
-        nn_prediction_results.nn_prediction_results[i - 1],
-        current_expected_output,
-        nn_instance.bias[i],
-        activation_function_value,
-        activation_function_derivative_value
-      );
-      matrix weights_gradient = _compute_weights_gradient(
-        nn_instance.weights[i],
-        nn_prediction_results.nn_prediction_results[i - 1],
-        current_expected_output,
-        nn_instance.bias[i],
-        activation_function_value,
-        activation_function_derivative_value
-      );
-      vec_scalar_multiply_inplace(&bias_gradient, epsilon);
-      matrix_scalar_multiply_inplace(&weights_gradient, epsilon);
-      vec_subtract_inplace(&nn_instance.bias[layers_count], bias_gradient);
-      matrix_subtract_inplace(&nn_instance.weights[layers_count], weights_gradient);
-
-      vec_assign(&current_expected_output,
-        vec_subtract(nn_prediction_results.nn_prediction_results[i - 1], signal_gradient));
-      vec_free(signal_gradient);
-      vec_free(bias_gradient);
-      matrix_free(weights_gradient);
-    }
-    {
-      const vec activation_function_value = nn_prediction_results.nn_prediction_results[0];
-      const matrix activation_function_derivative_value = nn_prediction_results.nn_transform_derivative_values[0];
-      vec bias_gradient = _compute_bias_gradient(
-        nn_instance.weights[0],
-        input,
-        current_expected_output,
-        nn_instance.bias[0],
-        activation_function_value,
-        activation_function_derivative_value
-      );
-      matrix weights_gradient = _compute_weights_gradient(
-        nn_instance.weights[nn_instance.layers_count],
-        nn_prediction_results.nn_prediction_results[nn_instance.layers_count - 1],
-        current_expected_output,
-        nn_instance.bias[nn_instance.layers_count],
-        activation_function_value,
-        activation_function_derivative_value
-      );
-      vec_scalar_multiply_inplace(&bias_gradient, epsilon);
-      matrix_scalar_multiply_inplace(&weights_gradient, epsilon);
-      vec_subtract_inplace(&nn_instance.bias[layers_count], bias_gradient);
-      matrix_subtract_inplace(&nn_instance.weights[layers_count], weights_gradient);
-
-      vec_free(bias_gradient);
-      matrix_free(weights_gradient);
-    }
-  } else {
-    const matrix weights = nn_instance.weights[0];
-    const vec bias = nn_instance.bias[0];
-    const vec activation_function_value = nn_prediction_results.nn_prediction_results[0];
-
+  for (int i = (int)layers_count - 1; i >= 0; --i) {
+    const vec activation_function_value = nn_prediction_results.nn_prediction_results[i];
+    const matrix activation_function_derivative_value = nn_prediction_results.nn_transform_derivative_values[i];
+    const vec signal_value = i > 0 ? nn_prediction_results.nn_prediction_results[i - 1] : input;
+    const matrix weight = nn_instance.weights[i];
+    const vec bias = nn_instance.bias[i];
     vec bias_gradient = _compute_bias_gradient(
-      weights,
-      input,
+      weight,
+      signal_value,
       current_expected_output,
       bias,
       activation_function_value,
-      identity
+      activation_function_derivative_value
     );
-
-    matrix weights_gradient = _compute_weights_gradient(
-      weights,
-      input,
+    vec signal_gradient = _compute_signal_gradient(
+      weight,
+      signal_value,
       current_expected_output,
       bias,
       activation_function_value,
-      identity
+      activation_function_derivative_value
+    );
+    matrix weights_gradient = _compute_weights_gradient(
+      weight,
+      signal_value,
+      current_expected_output,
+      bias,
+      activation_function_value,
+      activation_function_derivative_value
     );
     vec_scalar_multiply_inplace(&bias_gradient, epsilon);
     matrix_scalar_multiply_inplace(&weights_gradient, epsilon);
-    vec_subtract_inplace(&nn_instance.bias[0], bias_gradient);
-    matrix_subtract_inplace(&nn_instance.weights[0], weights_gradient);
+    vec_subtract_inplace(&nn_instance.bias[i], bias_gradient);
+    matrix_subtract_inplace(&nn_instance.weights[i], weights_gradient);
 
+    if (i > 0) {
+      vec_assign(&current_expected_output,
+        vec_subtract(nn_prediction_results.nn_prediction_results[i - 1], signal_gradient));
+    }
+    vec_free(signal_gradient);
     vec_free(bias_gradient);
     matrix_free(weights_gradient);
   }
   
   vec_free(current_expected_output);
   _nn_free_prediction_result_for_training(&nn_prediction_results, layers_count);
-  matrix_free(identity);
 }
 
 void nn_fit(nn nn_instance, const vec input, const vec expected_output, unsigned int training_cycles) {
@@ -374,17 +275,13 @@ void nn_fit(nn nn_instance, const vec input, const vec expected_output, unsigned
 vec nn_predict(const nn nn_instance, const vec input) {
   const unsigned int layers_count = nn_instance.layers_count;
   vec buffer = vec_copy(input);
-  for (unsigned int i = 0; i < nn_instance.layers_count; ++i) {
-    matrix weights = nn_instance.weights[i];
-    vec bias = nn_instance.bias[i];
-    nn_layer layer = nn_instance.layers[i];
+  for (unsigned int i = 0; i < layers_count; ++i) {
+    const matrix weights = nn_instance.weights[i];
+    const vec bias = nn_instance.bias[i];
+    const nn_layer layer = nn_instance.layers[i];
     matrix_apply_inplace(weights, &buffer);
     vec_sum_inplace(&buffer, bias);
     vec_assign(&buffer, layer.transform(layer.input_size, layer.output_size, buffer));
   }
-  matrix weights = nn_instance.weights[layers_count];
-  vec bias = nn_instance.bias[layers_count];
-  matrix_apply_inplace(weights, &buffer);
-  vec_sum_inplace(&buffer, bias);
   return buffer;
 }
